67ff37da04dcd06d06a9f28258bebc080f15e8eb
Prashanth Sheshappa <prashanth.hsn@gmail.com>
2022-06-19 21:43:20 +0200

    stack problems solutions.

--- epi_judge_cpp/directory_path_normalization.cc
+++ epi_judge_cpp/directory_path_normalization.cc
@@ -1,10 +1,54 @@
 #include <string>
+#include <vector>
+#include<sstream>
 
 #include "test_framework/generic_test.h"
+
 using std::string;
+using std::vector;
 string ShortestEquivalentPath(const string& path) {
-  // TODO - you fill in here.
-  return "";
+  
+    if (path.empty()) {
+        throw std::invalid_argument("Empty string!");
+    }
+
+    vector<string> path_names;
+    std::istringstream ss{ path };
+
+    if (path.front() == '/') {
+        path_names.push_back("/");
+    }
+    string token;
+    while (std::getline(ss, token, '/')) {
+        if (token == "..") {
+            if (path_names.empty() || path_names.back() == "..") {
+                path_names.push_back(token);
+            }
+            else {
+                if (path_names.back() == "/") {
+                    throw std::invalid_argument("Path error!");
+                }
+                path_names.pop_back();
+            }
+        }
+        else if ((token != ".") && (token != "")) {
+            path_names.push_back(token);
+        }
+    }
+    string result{};
+    if (!path_names.empty()) {
+        result = path_names[0];
+        for (int i = 1; i < path_names.size(); ++i) {
+            if (i == 1 && result == "/") {
+                result += path_names[i];
+            }
+            else {
+                result += "/" + path_names[i];
+            }
+        }
+    }
+
+  return result;
 }
 
 int main(int argc, char* argv[]) {
--- epi_judge_cpp/sunset_view.cc
+++ epi_judge_cpp/sunset_view.cc
@@ -1,13 +1,35 @@
 #include <iterator>
 #include <vector>
+#include <stack>
 
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::stack;
 vector<int> ExamineBuildingsWithSunset(
     vector<int>::const_iterator sequence_begin,
     const vector<int>::const_iterator& sequence_end) {
-  // TODO - you fill in here.
-  return {};
+  
+    struct BuildHeightIndex{
+        int idx;
+        int height;
+    };
+    stack<BuildHeightIndex> candidates;
+    int index = 0;
+    while (sequence_begin != sequence_end) {
+        int height = *sequence_begin++;
+        while (!candidates.empty() && (candidates.top().height <= height)) {
+            candidates.pop();
+        }
+        candidates.emplace(index, height);
+        index++;
+    }
+
+    vector<int> result;
+    while (!candidates.empty()) {
+        result.push_back(candidates.top().idx);
+        candidates.pop();
+    }
+  return result;
 }
 vector<int> ExamineBuildingsWithSunsetWrapper(const vector<int>& sequence) {
   return ExamineBuildingsWithSunset(cbegin(sequence), cend(sequence));
